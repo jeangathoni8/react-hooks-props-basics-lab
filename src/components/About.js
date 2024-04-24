1ecdad6936e2e296d4542886db0aaab19f7eca5d
jeangathoni8 <jeangathoni39@gmail.com>
2024-04-24 12:41:15 +0300

    final changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links"; // Ensure the Links component is imported
 
-function About() {
+function About({ bio, links }) { // Accept bio and links as props
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && <p>{bio}</p>} // Conditional rendering of the bio
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
+
+
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+import user from "../data/user"; // Importing the user data
 
 function App() {
+  const { name, city, color, bio, links } = user; // Destructuring user data
+
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} city={city} color={color} />
+      <About bio={bio} links={links} /> // Passing bio and links to About
     </div>
   );
 }
 
 export default App;
+
+
